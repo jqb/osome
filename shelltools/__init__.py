b1198ceced02245dda76e8eb88d36a127804f6be
xando <sebastian.pawlus@gmail.com>
2013-01-13 16:56:57 +0000

    Version check fixed

--- shelltools/__init__.py
+++ shelltools/__init__.py
@@ -1,9 +1,9 @@
 import sys
 
 if sys.version.startswith('3'):
-    base_string_class = unicode
-else:
     base_string_class = str
+else:
+    base_string_class = unicode
 
 from shelltools.run import run
 from shelltools.path import path
